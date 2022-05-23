86dbb2c6deba0abdf373a2ef4d1e305c325475f9
Nikolas Branis <nbran@kanenas.net>
2022-05-23 16:46:05 +0300

    Create pay_in_store.php

---
+++ upload/catalog/language/en-gb/extension/payment/pay_in_store.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title']       = 'Pay In Store';
+$_['text_instruction'] = 'Instructions for Pay In Store';
+$_['text_description'] = '';
+$_['text_payment']     = 'Visit one of our stores to complete your payment.';
